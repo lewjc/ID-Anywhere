691b09085201d16daaf5b71c6f8195424c11e025
Lewis Cummins <lewisjcummins@hotmail.co.uk>
2020-02-01 15:12:30 +0000

    Create signup

--- src/flutter/id_anywhere/lib/view/signup.dart
+++ src/flutter/id_anywhere/lib/view/signup.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:id_anywhere/widgets/ida_button.dart';
+import 'package:id_anywhere/widgets/ida_textinput.dart';
+import 'package:id_anywhere/widgets/ida_title.dart';
 
 class SignupPage extends StatefulWidget {
   SignupPage({Key key, this.title}) : super(key: key);
@@ -6,47 +9,123 @@ class SignupPage extends StatefulWidget {
   final String title;
 
   @override
-  _SignupState createState() => _SignupState();
+  State createState() => _SignupPageState();
 }
 
-class _SignupState extends State<SignupPage> {
+class _SignupPageState extends State<SignupPage> {
   final _formKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  void submit() {
+    _formKey.currentState.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return (Form(
-      key: _formKey,
-      child: Scaffold(
-        backgroundColor: Colors.pink,
-        body: Column(        
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                TextFormField( 
-                  decoration: InputDecoration(labelText: 'First name'),
-                ),
-                TextFormField(
-                  key: _formKey,
-                  decoration: InputDecoration(labelText: 'Last name'),
-                )
-              ],
-            ),
-            SizedBox(height: 100),
-            Row(
-              children: <Widget>[
-                TextFormField(
-                  key: _formKey,
-                  decoration: InputDecoration(labelText: 'First name'),
-                ),
-                TextFormField(
-                  key: _formKey,
-                  decoration: InputDecoration(labelText: 'Last name'),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-    ));
+    return Form(
+        key: _formKey,
+        child: Scaffold(
+          backgroundColor: Colors.pink,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  IDAnywhereTitle(text: 'Sign up'),
+                ],
+              ),
+              Spacer(flex: 3),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      IDAnywhereTextField(
+                        hint: 'First Name',
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter first name';
+                          }
+
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      IDAnywhereTextField(
+                        hint: 'Last Name',
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter last name';
+                          }
+
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  IDAnywhereTextField(
+                      hint: 'Email',
+                      validator: (value) => 'true',
+                      width: 300,
+                      controller: emailController),
+                  SizedBox(height: 20),
+                  IDAnywhereTextField(
+                    hint: 'Confirm email',
+                    validator: (value) => 'true',
+                    width: 300,
+                  ),
+                  SizedBox(height: 20),
+                  IDAnywhereTextField(
+                      hint: 'Password',
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter password';
+                        }
+
+                        return null;
+                      },
+                      width: 300,
+                      passwordField: true,
+                      controller: passwordController),
+                  SizedBox(height: 20),
+                  IDAnywhereTextField(
+                    hint: 'Confirm Password',
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please confirm password';
+                      } else if (value.toLowerCase() !=
+                          this.passwordController.text.toLowerCase()) {
+                        return 'Password does not match';
+                      } else {
+                        return null;
+                      }
+                    },
+                    width: 300,
+                    passwordField: true,
+                  ),
+                ],
+              ),
+              Spacer(
+                flex: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  IDAnywhereButton(text: "Submit", onPressed: submit),
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
+        ));
   }
 }
