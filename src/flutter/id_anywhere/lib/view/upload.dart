b1c041417a4d70c261b7768efa99758abdf8094c
Lewis Cummins <lewisjcummins@hotmail.co.uk>
2020-02-06 19:41:23 +0000

    Add base homepage files

---
+++ src/flutter/id_anywhere/lib/view/upload.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class UploadPage extends StatefulWidget{
+ UploadPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State createState() => _UploadPageState();
+
+
+}
+
+class _UploadPageState extends State<UploadPage>{
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
