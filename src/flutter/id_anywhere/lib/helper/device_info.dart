8c7df3ee5595ef617bacd1077ab0ed1db8088c02
Lewis Cummins <lewisjcummins@hotmail.co.uk>
2020-02-06 19:40:34 +0000

    Update helper methods

--- src/flutter/id_anywhere/lib/helper/device_info.dart
+++ src/flutter/id_anywhere/lib/helper/device_info.dart
@@ -1,20 +1,20 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:crypto/crypto.dart';
 import 'package:device_info/device_info.dart';
 
-class DeviceInfoHelper{
-
-  static String getDeviceId(){
+class DeviceInfoHelper {
+  static Future<String> getDeviceId() async{
     String id;
     if (Platform.isAndroid) {
-          DeviceInfoPlugin().androidInfo.then((build) {
-            id = build.androidId;
-          });
-        } else {
-          DeviceInfoPlugin().iosInfo.then((build) {
-            id = build.identifierForVendor;
-          });
-        }
+      id = (await DeviceInfoPlugin().androidInfo).androidId;
+    } else {
+      id = (await DeviceInfoPlugin().iosInfo).identifierForVendor;
+    }
     return id;
   }
-}     
-    
+
+  static String hashId(String id) {
+    return sha256.convert(utf8.encode(id)).toString();
+  }
+}
