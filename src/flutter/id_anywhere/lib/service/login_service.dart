8ff6bb680fb7602c9f84d6c1a526553b8fb522dc
Lewis Cummins <lewisjcummins@hotmail.co.uk>
2020-02-13 17:01:01 +0000

    Initialise session on login

--- src/flutter/id_anywhere/lib/service/login_service.dart
+++ src/flutter/id_anywhere/lib/service/login_service.dart
@@ -10,45 +10,54 @@ import 'package:id_anywhere/helper/http.dart';
 import 'package:id_anywhere/models/login_model.dart';
 import 'package:id_anywhere/service/service_registration.dart';
 import 'package:id_anywhere/service/service_result.dart';
+import 'package:id_anywhere/state/session.dart';
+import 'package:id_anywhere/state/session_user.dart';
 
 class LoginService {
   Future<ServiceResult> executeLogin(String email, String password) async {
     var appId = DeviceInfoHelper.hashId(await DeviceInfoHelper.getDeviceId());
-    LoginModel model = LoginModel(email: email, password: password, appID: appId);
+    LoginModel model =
+        LoginModel(email: email, password: password, appID: appId);
     if (model.isValid()) {
       String url = GlobalConfiguration().getString("api_url");
       url = "$url/api/account/login";
-      try{
+      try {
         Response response = await HttpHelper.postJson(model.toJson(), url);
         return handleResponse(response);
-      } on Exception catch(e){
+      } on Exception catch (e) {
         print(e);
-      }      
+      }
     }
-    
+
     return ServiceResult(errors: ["Invalid email or password"]);
   }
 
-  Future<ServiceResult> handleResponse(Response response) async{
+  Future<ServiceResult> handleResponse(Response response) async {
     ServiceResult result = ServiceResult();
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       // Login successful, store JWT and then login to home.
       var responseData = json.decode(response.body);
       String token = responseData["token"];
+      final session = resolver<Session>();
+      session.token = token;
+      session.user = new SessionUser(
+          firstname: responseData["firstname"], status: responseData["status"]);
       // Return a token, firstname and status. This can be stored in the current user's session.
       resolver<FlutterSecureStorage>().write(key: Flags.jwt, value: token);
       return result;
-    } else{
+    } else {
       // Invalid username or password
       result.errors = json.decode(response.body)["errors"];
       return result;
     }
   }
 
-  Future<ServiceResult> loginFromLocalAuth() async{
+  Future<ServiceResult> loginFromLocalAuth() async {
     final connection = resolver<FirebaseConnection>();
-    final idHash = DeviceInfoHelper.hashId(await DeviceInfoHelper.getDeviceId());
-    DocumentSnapshot document = await (await connection.getUserInfoReference()).document(idHash).get();    
+    final idHash =
+        DeviceInfoHelper.hashId(await DeviceInfoHelper.getDeviceId());
+    DocumentSnapshot document =
+        await (await connection.getUserInfoReference()).document(idHash).get();
     String email = document.data["email"];
     String password = document.data["password"];
     return await executeLogin(email, password);
