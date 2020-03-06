4ef64e02e67306f705ce800e0a3f6cccadc8fe2a
lewjc <lewisjcummins@hotmail.co.uk>
2020-03-06 15:41:19 +0000

    Add web app initial files

---
+++ src/webapp/IdAnywhereWebApplication/WebApp/Results/LoginResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebApp.Results
+{
+  public class LoginResult
+  {
+    [JsonPropertyName("firstname")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("token")]
+    public string Token { get; set; }
+
+    [JsonPropertyName("status")]
+    public string Status { get; set; }
+    public bool IsValid { get { return Errors.Count == 0; } }
+    public List<string> Errors { get; set; } = new List<string>();
+  }
+}
---
+++ src/webapp/IdAnywhereWebApplication/WebApp/Services/IJobService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+  public interface IJobService
+  {
+    Task<List<JobViewModel>> LoadJobs(int page, int size, string bearer);
+
+    Task<JobViewModel> GetJob(int Id, string bearerToken);
+
+    Task<bool> UpdateJob(JobViewModel model, string bearerToken);
+  }
+}
